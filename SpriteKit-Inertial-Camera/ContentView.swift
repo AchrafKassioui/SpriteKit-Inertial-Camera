fb662f7055a03ebb7bd6209e6e0e71e8e663f42f
Achraf Kassioui <achrafkassioui@gmail.com>
2024-04-09 21:51:04 +0000

    Initial Commit

---
+++ SpriteKit-Inertial-Camera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpriteKit-Inertial-Camera
+//
+//  Created by Achraf Kassioui on 9/4/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpriteKit-Inertial-Camera/SpriteKit_Inertial_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKit_Inertial_CameraApp.swift
+//  SpriteKit-Inertial-Camera
+//
+//  Created by Achraf Kassioui on 9/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKit_Inertial_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
